5cb0769b5ea859d677d6ddccfbb091d469d79ec9
Leonov Egor <eileonov@gmail.com>
2023-05-16 23:29:05 +0300

    Finish

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
+    private static final Logger loger = LogManager.getLogger(Main.class);
     public static void main(String[] args) throws Exception {
         List<Horse> horses = List.of(
                 new Horse("Буцефал", 2.4),
@@ -14,6 +20,7 @@ public class Main {
                 new Horse("Вишня", 3)
         );
         Hippodrome hippodrome = new Hippodrome(horses);
+        loger.info("Начало скачек. Количество участников: "+horses.size());
 
         for (int i = 0; i < 100; i++) {
             hippodrome.move();
@@ -23,6 +30,7 @@ public class Main {
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println("Победил " + winnerName + "!");
+        loger.info("Окончание скачек. Победитель: "+winnerName);
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
